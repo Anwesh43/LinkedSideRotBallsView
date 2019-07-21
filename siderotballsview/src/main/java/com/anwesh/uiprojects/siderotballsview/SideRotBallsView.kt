1a6522cd7f9b3614d8c2677f453fc3a1108202c8
Anwesh43 <anweshthecool0@gmail.com>
2019-07-21 13:42:02 +0530

    adding Int and Float extension methods

--- siderotballsview/src/main/java/com/anwesh/uiprojects/siderotballsview/SideRotBallsView.kt
+++ siderotballsview/src/main/java/com/anwesh/uiprojects/siderotballsview/SideRotBallsView.kt
@@ -14,9 +14,19 @@ import android.app.Activity
 
 val nodes : Int = 5
 val scGap : Float = 0.05f
-val scDiv : Float = 90f
+val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 0.05f
 val foreColor : Int = Color.GREEN
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
