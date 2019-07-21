4d8668c0e6cf2414d45d5c0e106029937b88e43d
Anwesh43 <anweshthecool0@gmail.com>
2019-07-21 13:37:21 +0530

    adding constants and libraries

--- siderotballsview/src/main/java/com/anwesh/uiprojects/siderotballsview/SideRotBallsView.kt
+++ siderotballsview/src/main/java/com/anwesh/uiprojects/siderotballsview/SideRotBallsView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.siderotballsview
  * Created by anweshmishra on 21/07/19.
  */
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val scGap : Float = 0.05f
+val scDiv : Float = 90f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 0.05f
+val foreColor : Int = Color.GREEN
+val backColor : Int = Color.parseColor("#BDBDBD")
+
