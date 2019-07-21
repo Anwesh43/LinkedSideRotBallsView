a14783e8e68d32152999fff3b130bf47b733630c
Anwesh43 <anweshthecool0@gmail.com>
2019-07-21 14:44:27 +0530

    adding SideRotBallsRotView class

--- siderotballsview/src/main/java/com/anwesh/uiprojects/siderotballsview/SideRotBallsView.kt
+++ siderotballsview/src/main/java/com/anwesh/uiprojects/siderotballsview/SideRotBallsView.kt
@@ -65,3 +65,21 @@ fun Canvas.drawSRBNode(i : Int, scale : Float, paint : Paint) {
     restore()
     restore()
 }
+
+class SideRotBallsView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
